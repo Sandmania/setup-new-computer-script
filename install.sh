ce06cf5055ed510820e89d64f3052ee0e87f1315
Jouni Latvatalo <jouni.latvatalo@iki.fi>
2022-01-12 16:51:14 +0200

    Add preliminary install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script should be run via curl:
+#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/Sandmania/setup-new-computer-script/master/install.sh)"
+set -euo
+git clone https://github.com/Sandmania/setup-new-computer-script.git
+cd setup-new-computer-script
+git checkout sandman-specific
+./setup-new-computer.sh
